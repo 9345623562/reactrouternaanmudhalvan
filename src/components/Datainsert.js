ed6b1fab1e4466723dcacf66a6aec800ec5aec60
9345623562 <snehag5602@gmail.com>
2026-01-06 13:16:37 -0800

    Array Msg

--- src/components/Datainsert.js
+++ src/components/Datainsert.js
@@ -1,9 +1,56 @@
 import React from 'react'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const DataInsert = () => {
-  return (
+  const navigate=useNavigate()
+  const [rno,setRno]=useState("")
+  const [sname,setSname]=useState("")
+  const [mark,setMark]=useState("")
+  const [rank,setRank]=useState("")
+  const [finres,setFinres]=useState([])
+  const dis=(event)=>{
+    if(event.target.id==="rno")
+      setRno(event.target.value)
+    if(event.target.id==="sname")
+      setSname(event.target.value)
+    if(event.target.id==="mark")
+      setMark(event.target.value)
+    if(event.target.id==="rank")
+      setRank(event.target.value)
+  }
+  const result=()=>{
+    const resjson={
+      rno:rno,
+      sname:sname,
+      mark:mark,
+      rank:rank,
+    }
+    console.log("my json:",resjson)
+    setFinres([resjson,...finres])
+  }
+    return (
     <div>
-      <h2>This is DataInsert us compoments</h2>
+      <h1>Student Information-Insert/Save Form</h1><br></br>
+      <input type="text" id="rno" onChange={(ev)=>dis(ev)} value={rno} placeholder='Enter Roll Number:'></input><br></br>
+      <input type="text" id="sname" onChange={(ev)=>dis(ev)} value={sname} placeholder='Enter Student Name:'></input><br></br>
+      <input type="text" id="mark" onChange={(ev)=>dis(ev)} value={mark} placeholder='Enter Mark:'></input><br></br>
+      <input type="text" id="rank" onChange={(ev)=>dis(ev)} value={rank} placeholder='Enter Rank:'></input><br></br>
+      <button onClick={result}>Store to array</button><br></br>
+      <button onClick={()=>navigate("/View")}>Goto View Component</button><br></br>
+      <hr size="2" color="blue" ></hr>
+       <h1>Student Mark list</h1> 
+      <hr size="2" color="blue" ></hr>
+      {finres.map((item)=>
+      <>
+     
+      Roll Number: {item.rno}<br></br>
+      Student Name:{item.sname}<br></br>
+      Student Mark:{item.mark}<br></br>
+      Student Rank:{item.rank}<br></br>
+      <hr size="2" color='blue' width="50%" align="left"></hr>
+      </>
+      )}
     </div>
   )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,15 @@
 import { NavLink } from 'react-router-dom'
 import React from 'react'
+import "../App.css"
 
 const Navbar = () => {
   return (
     <>
     
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/View">View</NavLink>
-        <NavLink to="/DataInsert">DataInsert</NavLink>
-        <NavLink to="/Help">Help</NavLink>
+        <NavLink id="n1" to="/">Home</NavLink>
+        <NavLink id="n1" to="/View">View</NavLink>
+        <NavLink id="n1" to="/DataInsert">DataInsert</NavLink>
+        <NavLink id="n1" to="/Help">Help</NavLink>
     
     </>
   )
